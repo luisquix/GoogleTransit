8c3cfed602c5a8cf19916e0662d9cf004bcb5913
Luis <luis@quix.io>
2024-02-08 13:27:09 +0100

    Simple transformation

--- ArrivalDetection/main.py
+++ ArrivalDetection/main.py
@@ -1,17 +1,20 @@
 import os
 from quixstreams import Application, State
 from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer
+# import the dotenv module to load environment variables from a file
+#from dotenv import load_dotenv
+#load_dotenv(override=False)
 
+app = Application.Quix("TrainData", auto_offset_reset="latest")
 
-app = Application.Quix("transformation-v1", auto_offset_reset="latest")
-
-input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
-output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
+input_topic = app.topic(os.environ["LiveTrainData"])
+output_topic = app.topic(os.environ["ArrivalTrainData"])
 
 sdf = app.dataframe(input_topic)
 
 # Here put transformation logic.
 
+sdf = sdf[(sdf["progress"] > 0.9)]
 sdf = sdf.update(lambda row: print(row))
 
 sdf = sdf.to_topic(output_topic)
